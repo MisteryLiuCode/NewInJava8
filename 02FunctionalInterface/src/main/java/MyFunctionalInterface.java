8dbcdc78f2f49f817f0c0d14a0f4d0ddb728cf1b
liu <liushuaibiao@jiexingcloud.com>
2022-06-05 10:23:22 +0800

    函数式接口简介和自定义函数式接口

---
+++ 02FunctionalInterface/src/main/java/MyFunctionalInterface.java
@@ -0,0 +1,10 @@
+/**
+ * @program: NewInJava8
+ * @description: 函数式接口
+ * @author: 刘帅彪
+ * @create: 2022-06-05 10:18
+ **/
+@FunctionalInterface
+public interface MyFunctionalInterface {
+    void method();
+}
